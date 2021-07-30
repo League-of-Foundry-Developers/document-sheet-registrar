8e35ba8d03908ddcef207ea8c9b75ef723bf5bdb
zeel <zeel02@gmail.com>
2021-07-30 14:06:58 -0400

    Generalized to add registration methods and handling to any document type

--- scripts/document-sheet-registrar.js
+++ scripts/document-sheet-registrar.js
@@ -1,5 +1,5 @@
 /**
- * Creates a shim that allows for the registration of custom Journal Entry sheets.
+ * Creates a shim that allows for the registration of custom document sheets.
  *
  * Requires libWrapper (or libWrapper shim).
  *
@@ -18,8 +18,138 @@
  * });
  *
  */
-function _initializeJournalSheetShim() {
-	function _getSheetClass() {
+class DocumentSheetRegistrar {
+	/**
+	 * @typedef {object} DocumentMap A map of document name, class, and collection
+	 * @property {string}              name       - The name of the document
+	 * @property {ClientDocumentMixin} class      - The class of the document
+	 * @property {WorldCollection}     collection - The collection of the document
+	 *//**
+	 * A mapping of document types and the classes that handle them.
+	 * @type {DocumentMap[]}
+	 */
+	static get documentTypes() {
+		return [
+			{ "name": "JournalEntry", "class": JournalEntry, "collection": Journal },
+			{ "name": "Macro",        "class": Macro,        "collection": Macros },
+			{ "name": "Scene",        "class": Scene,        "collection": Scenes },
+		]
+	}
+
+	
+	/**
+	 * Initialize all of the document sheet registrars.
+	 *
+	 * @static
+	 * @memberof DocumentSheetRegistrar
+	 */
+	static initializeDocumentSheets() {
+		for (let doc of this.documentTypes) {
+			// Skip any collection that already has a sheet registration method
+			if (doc.collection.registerSheet) continue;
+			
+			this.initializeDocumentSheet(doc);
+		}
+	}
+
+	
+	/**
+	 * Set up the nessesary configuration objects and methods for 
+	 * sheet registration of this document type.
+	 *
+	 * @static
+	 * @param {DocumentMap} doc - The type of document to add sheet registration to
+	 * @memberof DocumentSheetRegistrar
+	 */
+	static initializeDocumentSheet(doc) {
+		// Set the base type for this document class
+		doc.class.prototype.type = "base";
+
+		// Override the sheet retrieval method for this document type
+		libWrapper.register("_document-sheet-registrar", `${doc.name}.prototype._getSheetClass`, this._getSheetClass, "OVERRIDE");
+
+		// Add sheet registration methods to the document collection
+		this.addRegistrationMethods(doc);
+
+		// Configure the sheetClasses object for this document type
+		this.configureSheetClasses(doc);
+	}
+
+	
+	/**
+	 * Creates a new sheetClasses config object for this document type.
+	 *
+	 * @param {*} doc - The type of document to add a config object for
+	 * @memberof DocumentSheetRegistrar
+	 */
+	static configureSheetClasses(doc) {
+		CONFIG[doc.name].sheetClasses = {
+			"base": {                                // "base" because these documents only have one type
+				[doc.name]: {                        // Register the default sheet
+					id: doc.name,
+					default: true,                   // As the default
+					label: doc.name,
+					cls: CONFIG[doc.name].sheetClass
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Adds a register and unregister method to the document collection.
+	 *
+	 * @param {*} doc
+	 * @memberof DocumentSheetRegistrar
+	 */
+	static addRegistrationMethods(doc) {
+		/**
+		* Register a sheet class as a candidate which can be used to display this document.
+		*
+		* @param {string}      scope                  Provide a unique namespace scope for this sheet
+		* @param {Application} sheetClass             A defined Application class used to render the sheet
+		* @param {Object}      options                Additional options used for sheet registration
+		* @param {string}     [options.label]         A human readable label for the sheet name, which will be localized
+		* @param {string[]}   [options.types]         An array of entity types for which this sheet should be used
+		* @param {boolean}    [options.makeDefault]   Whether to make this sheet the default for provided types
+		*
+		* @example
+		* Document.registerSheet?.("myModule", SheetApplicationClass, {
+		*     types: ["base"],
+		*     makeDefault: false,
+		*     label: "My Document sheet"
+		* });
+		*/
+		doc.collection.registerSheet = function (...args) {
+			EntitySheetConfig.registerSheet(doc.class, ...args);
+		}
+
+		/**
+		* Unregister a sheet class, removing it from the list of available Applications to use for this document.
+		*
+		* @param {string} scope              Provide a unique namespace scope for this sheet
+		* @param {Application} sheetClass    A defined Application class used to render the sheet
+		* @param {Object} [options]          Additional options used for sheet registration
+		* @param {string[]} [options.types]  An Array of types for which this sheet should be removed
+		*
+		* @example
+		* Document.unregisterSheet?.("myModule", SheetApplicationClass, {
+		* 	types: ["base"],
+		* });
+		*/
+		doc.collection.unregisterSheet = function (...args) {
+			EntitySheetConfig.unregisterSheet(doc.class, ...args)
+		}
+	}
+
+	
+	/**
+	 * Retrieve the sheet class for the document. @see Actor._getSheetClass
+	 *
+	 * @static
+	 * @return {*} 
+	 * @memberof DocumentSheetRegistrar
+	 */
+	static _getSheetClass() {
 		const cfg = CONFIG[this.documentName];
 		const sheets = cfg.sheetClasses[this.type] || {};
 		const override = this.getFlag("core", "sheetClass");
@@ -28,60 +158,6 @@ function _initializeJournalSheetShim() {
 		if (!classes.length) return null;
 		return (classes.find(s => s.default) ?? classes.pop()).cls;
 	}
-
-	JournalEntry.prototype.type = "base";
-
-	libWrapper.register("_document-sheet-registrar", "JournalEntry.prototype._getSheetClass", _getSheetClass, "OVERRIDE");
-
-	/**
-	 * Register a sheet class as a candidate which can be used to display Journal Entries.
-	 *
-	 * @param {string} scope            Provide a unique namespace scope for this sheet
-	 * @param {Application} sheetClass  A defined Application class used to render the sheet
-	 * @param {Object} options          Additional options used for sheet registration
-	 * @param {string} [options.label]          A human readable label for the sheet name, which will be localized
-	 * @param {string[]} [options.types]        An array of entity types for which this sheet should be used
-	 * @param {boolean} [options.makeDefault]   Whether to make this sheet the default for provided types
-	 *
-	 * @example
-	 * Journal.registerSheet?.("myModule", SheetApplicationClass, {
-	 *     types: ["base"],
-	 *     makeDefault: false,
-	 *     label: "My Journal Entry sheet"
-	 * });
-	 */
-	Journal.registerSheet = function (...args) {
-		EntitySheetConfig.registerSheet(JournalEntry, ...args);
-	}
-
-	/**
-	 * Unregister a Journal Entry sheet class, removing it from the list of available Applications to use for Journal Entries.
-	 *
-	 * @param {string} scope            Provide a unique namespace scope for this sheet
-	 * @param {Application} sheetClass  A defined Application class used to render the sheet
-	 * @param {Object} [options]          Additional options used for sheet registration
-	 * @param {string[]} [options.types]             An Array of types for which this sheet should be removed
-	 *
-	 * @example
-	 * Journal.unregisterSheet?.("myModule", SheetApplicationClass, {
-	 * 	types: ["base"],
-	 * });
-	 */
-	Journal.unregisterSheet = function (...args) {
-		EntitySheetConfig.unregisterSheet(JournalEntry, ...args)
-	}
-
-	CONFIG.JournalEntry.sheetClasses = {
-		"base": {                                  // "base" because there is only one type of Journal
-			"JournalSheet": {                        // Register the default sheet
-				id: "JournalSheet",
-				default: true,                         // As the default
-				label: "JournalSheet",
-				cls: CONFIG.JournalEntry.sheetClass
-			}
-		}
-	}
 }
 
-
-Hooks.once("init", () => Journal.registerSheet ? null : _initializeJournalSheetShim());
+Hooks.once("init", DocumentSheetRegistrar.initializeDocumentSheets.bind(DocumentSheetRegistrar));
